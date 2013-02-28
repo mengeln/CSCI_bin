048bc336632e5ad3b0b356ac9e3a9674cece7b40
unknown <marke@Trestles.SCCWRP2k.lan>
2013-02-28 15:40:00 -0800

    added install script

---
+++ installCSCI.r
@@ -0,0 +1,26 @@
+
+installif <- function(x){
+  for(i in 1:length(x))
+    if(!require(package = x[i], character.only=TRUE)){
+      install.packages(x[i])
+      require(x[i], character.only=TRUE)
+    }
+}
+
+installif(c("randomForest", "vegan", "stringr",
+            "reshape2", "plyr", "data.table"))
+
+installCSCI <- function(){
+  tempdir <- tempdir()
+  tempfile <- paste0(tempdir, "\\cscibins.zip")
+  download.file("https://github.com/mengeln/CSCI_bin/archive/master.zip", tempfile)
+  unzip(tempfile, exdir = tempdir)
+  packages <- list.files(paste0(tempdir, "\\CSCI_bin-master"), full.names=TRUE)
+  install.packages(packages, repos=NULL)
+  unlink(packages)
+}
+
+installCSCI()
+
+rm(installif)
+rm(installCSCI)
